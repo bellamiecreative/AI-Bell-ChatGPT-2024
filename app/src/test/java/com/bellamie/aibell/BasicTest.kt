b42c730012e07c35c6add75b1b33ed32d826af1d
bellamiecreative <bellamiecreative@gmail.com>
2026-09-25 14:57:57 +0300

    Add files via upload

---
+++ app/src/test/java/com/bellamie/aibell/BasicTest.kt
@@ -0,0 +1,11 @@
+package com.bellamie.aibell
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class BasicTest {
+    @Test
+    fun appName_isStable() {
+        assertEquals("AI Bell", "AI Bell")
+    }
+}
